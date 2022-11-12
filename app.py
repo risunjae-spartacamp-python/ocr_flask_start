da6ff568d1671c34becad36fce7c3a11ee093fdc
risunjae121 <risunjae121@yahoo.co.jp>
2022-11-12 15:13:09 +0900

    mkdir ocr_flask_start

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+import pyocr.builders
+import pyocr
+from PIL import Image
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    # リクエストファイルの読み込み
+    ocr_file = request.files["ocrFile"]
+    img = Image.open(ocr_file)
+    # OCR準備
+    tools = pyocr.get_available_tools()
+    tool = tools[0]
+    # OCR
+    txt = tool.image_to_string(
+        img, lang="eng+jpn", builder=pyocr.builders.TextBuilder()
+    )
+
+    if txt == "":
+        txt = "読み取りエラー"
+        return render_template("index.html", txt=txt)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
